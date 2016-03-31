3aa8d42aaa179078031915aba0ab76acdf2db4b3
nUCastro <nucastro@uc.cl>
2016-03-31 15:42:07 -0300

    Added files via upload

---
+++ estadistica.py
@@ -0,0 +1,24 @@
+import numpy as np
+from pylab import *
+import matplotlib.pyplot as plt
+ls=[]
+#cambiar el numero 1000
+for i in range(1000):
+	l=[]
+	total=0
+	for j in range(1000):
+		l2=[]
+		for k in range(33):
+			l2.append(np.random.randint(2))
+		l.append(l2)
+		if sum(l2)==18:
+			total=total+1
+	
+	s=(total*1.0/1000)*100	
+	if i%100==0:
+		print s,'%   ', i
+	ls.append(s)
+
+plt.hist(ls)
+plt.show()	
+	
